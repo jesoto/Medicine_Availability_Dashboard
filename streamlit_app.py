f5d0b478633ffa4c2318c5c673918ffefd4b17dd
Jesús Soto <jesus.soto@pucp.edu.pe>
2024-08-01 02:06:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,10 @@
 # Import libraries 
 import streamlit as st 
 import pandas as pd 
-import folium 
-import altair as alt
-import plotly.express as px 
-import numpy as np
-from folium import Popup
+import geopandas as gpd
+import folium
+from streamlit_folium import st_folium
+
 #################################
 
 st.set_page_config(
@@ -36,24 +35,8 @@ with st.sidebar:
        'JUNIN', 'LAMBAYEQUE']
     
     selected_depart = st.selectbox('Selecciona el departamento', depart_list)
-    
-    
+
 #################################
-
-# Plots
-
-# Map 
-
-
-
-# lineplot
-
-
-
-
-# top desabastecido
-
-
 
 # Donut chart
 def assign_color(idm_value):
@@ -120,40 +103,91 @@ idm_anual_hosp = pd.read_excel('data/IDM_anual_hospitales.xlsx')
 idm_anual_cen = pd.read_excel('data/IDM_anual_centros.xlsx')
 idm_anual_pue = pd.read_excel('data/IDM_anual_puestos.xlsx')
 
+# map of IDM 
 
+dep = gpd.read_file('data/LIMITE_DEPARTAMENTO/LIMITE_DEP.shp')
+geo_idm = pd.read_excel('data/geo_idm_anual.xlsx')
 
+# Verificar si las columnas latitud y longitud existen y tienen datos válidos
+if 'latitud' not in geo_idm.columns or 'longitud' not in geo_idm.columns:
+    st.error("Las columnas 'latitud' y 'longitud' no existen en el DataFrame.")
+else:
+    if geo_idm[['latitud', 'longitud']].isnull().any().any():
+        st.error("Hay valores nulos en las columnas 'latitud' y 'longitud'.")
+    else:
+        ####################################
+        # Dashboard Main Panel
+        col = st.columns((1.5, 4.5, 2), gap='medium')
 
+        with col[0]:
+            st.markdown('#### IDM Anual Departamental')
+            
+            IDM_anual = calculate_idm_by_depart_year(idm_anual_data, selected_year, selected_depart)
+            IDM_anual_hosp = calculate_idm_by_depart_year(idm_anual_hosp, selected_year, selected_depart)
+            IDM_anual_cen = calculate_idm_by_depart_year(idm_anual_cen, selected_year, selected_depart)
+            IDM_anual_pue = calculate_idm_by_depart_year(idm_anual_pue, selected_year, selected_depart)
 
+            idm_donut_total_chart = make_donut(IDM_anual, selected_depart)
+            idm_donut_hosp_chart = make_donut(IDM_anual_hosp, selected_depart)
+            idm_donut_cen_chart = make_donut(IDM_anual_cen, selected_depart)
+            idm_donut_pue_chart = make_donut(IDM_anual_pue, selected_depart)
+            
+            st.write('IDM Anual Total')
+            st.altair_chart(idm_donut_total_chart, use_container_width=True)
+            
+            st.write('IDM Anual Total - Hospitales')
+            st.altair_chart(idm_donut_hosp_chart, use_container_width=True)
+            
+            st.write('IDM Anual Total - Centros')
+            st.altair_chart(idm_donut_cen_chart, use_container_width=True)
+            
+            st.write('IDM Anual Total - Puestos')
+            st.altair_chart(idm_donut_pue_chart, use_container_width=True)
 
-####################################
-# Dashboard Main Panel
-col = st.columns((1.5,4.5,2), gap='medium')
+        with col[1]:
+            # Filtrar datos según el año y el departamento seleccionados
+            filtered_data = geo_idm[(geo_idm['año'] == selected_year) & (geo_idm['departamento'] == selected_depart)]
 
-with col[0]:
-    st.markdown('#### IDM Anual Departamental')
-    
-    IDM_anual = calculate_idm_by_depart_year(idm_anual_data,selected_year, selected_depart)
-    IDM_anual_hosp = calculate_idm_by_depart_year(idm_anual_hosp, selected_year, selected_depart)
-    IDM_anual_cen = calculate_idm_by_depart_year(idm_anual_cen, selected_year, selected_depart)
-    IDM_anual_pue = calculate_idm_by_depart_year(idm_anual_pue, selected_year, selected_depart)
+            # Crear el mapa de Folium
+            m = folium.Map(location=[-9.19, -75.0152], tiles='cartodbpositron', zoom_start=7)
 
-    
-    
-    
-    idm_donut_total_chart = make_donut(IDM_anual,selected_depart)
-    idm_donut_hosp_chart = make_donut(IDM_anual_hosp,selected_depart)
-    idm_donut_cen_chart = make_donut(IDM_anual_cen,selected_depart)
-    idm_donut_pue_chart = make_donut(IDM_anual_pue,selected_depart)
-    
-    
-    st.write('IDM Anual Total')
-    st.altair_chart(idm_donut_total_chart, use_container_width=True)
-    
-    st.write('IDM Anual Total - Hospitales')
-    st.altair_chart(idm_donut_hosp_chart, use_container_width=True)
-    
-    st.write('IDM Anual Total - Centros')
-    st.altair_chart(idm_donut_cen_chart, use_container_width=True)
-    
-    st.write('IDM Anual Total - Puestos')
-    st.altair_chart(idm_donut_pue_chart, use_container_width=True)
+            # Crear grupos de capas para los diferentes tipos de establecimientos
+            hospital_layer = folium.FeatureGroup(name="Hospital")
+            centro_layer = folium.FeatureGroup(name="Centro de salud")
+            puesto_layer = folium.FeatureGroup(name="Puesto de Salud")
+            otro_layer = folium.FeatureGroup(name="Otro")
+
+            for idx, row in filtered_data.iterrows():
+                nombre = row['establec']
+                tipo_establecimiento = row['tipo']
+                disponibilidad = row['dispo']
+                lat = row['latitud']
+                lon = row['longitud']
+
+                # Crear contenido HTML para el popup
+                popup_content = f"""
+                <b>Nombre:</b> {nombre}<br>
+                <b>Tipo de Establecimiento:</b> {tipo_establecimiento}<br>
+                <b>Disponibilidad:</b> {disponibilidad}%
+                """
+                popup = folium.Popup(popup_content, max_width=300)
+
+                marker = folium.CircleMarker(location=[lat, lon], radius=5, popup=popup, fill=True)
+
+                if tipo_establecimiento == "Hospital":
+                    marker.add_to(hospital_layer)
+                elif tipo_establecimiento == "Centro de salud":
+                    marker.add_to(centro_layer)
+                elif tipo_establecimiento == "Puesto de Salud":
+                    marker.add_to(puesto_layer)
+                else:
+                    marker.add_to(otro_layer)
+
+            hospital_layer.add_to(m)
+            centro_layer.add_to(m)
+            puesto_layer.add_to(m)
+            otro_layer.add_to(m)
+
+            folium.LayerControl().add_to(m)
+
+            st_folium(m, width=700, height=500)
